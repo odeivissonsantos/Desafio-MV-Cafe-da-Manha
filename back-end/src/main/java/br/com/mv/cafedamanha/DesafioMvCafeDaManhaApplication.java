a4eb5dbad88a6612651c82323c3dd1f8d7878241
Deivisson Santos <deivissonsantos@hotmail.com>
2021-10-20 18:00:48 -0300

    iniciando o back end

---
+++ back-end/src/main/java/br/com/mv/cafedamanha/DesafioMvCafeDaManhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mv.cafedamanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMvCafeDaManhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMvCafeDaManhaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/br/com/mv/cafedamanha/DesafioMvCafeDaManhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.cafedamanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMvCafeDaManhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
